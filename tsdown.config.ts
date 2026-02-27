6ba508f8c4ead5a8668c2843a8dc129c662c2836
yel <yel@sucsoft.com>
2026-02-27 14:13:56 +0800

    chore: add type

--- tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown';
-
-export default defineConfig({
-    entry: ['./compress.ts'],
-    target: ['node20'],
-    exports: true,
-    clean: false,
-    outDir: '.',
-});
